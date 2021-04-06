34d1ba205009e35dae7962a48ba58c03848d06c4
Laëtitia PHIMMASANE <laetitia.phimmasane@epsi.fr>
2021-04-06 18:10:25 +0200

    erreurs, gestion du token

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # api-rest-justify-text
+
+# introduction
+An API REST (Application Programming Interface) is a set of paths used for many web applications and mobile app. It is an equivalent of a remote control for a TV. We send a request, we've got a response according to what the user want. 
+
+Here, I have developped an API REST, where we have as an input a text, non-justified and the output is the text justified. 
+
+We have two paths /api/justify to do the main part of the API and /api/token to authentify the user.
--- index.js
+++ index.js
@@ -2,22 +2,28 @@
 var express = require('express');
 var app = express();
 
+//Gestion du token
 var jwt = require('jsonwebtoken');
-const privateKey = 'foJOGIROIJDSOIofjeoieGRIOZOEHHF';
+var privateKey = "foJIJDSO\nIofjeoieG14FHHFy\n";
 
+
+//Gérer de la lecture des body
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json());
 
 //Route GET
 app.get('/api', (req,res) => {
     res.send("L'API REST bonjour.")
 });
 
-//Route POST /api/justify
 app.post('/api/justify', (req,res) => {
     try {
     //request
     req.token
+    //vérifier que le token 
     console.log('Voici le body',req.body);
-
+    
     //response
         //appel de la fonction
 
@@ -25,27 +31,34 @@ app.post('/api/justify', (req,res) => {
         res.send('404 error');
     }
     res.sendStatus(200);
-    res.header('text/plain');
+    res.SetHeader('Content-Type','text/plain');
 });
 
 //Route POST /api/token
 app.post('/api/token', (req,res) => {
     //request
-    console.log(req.user);
-    console.log(req.password == "user");
-    if (req.user == "user" && req.password == "user") {
-        var token = jwt.sign({ user: req.user}, privateKey, { algorithm : 'RS256', expiresIn : 10000});
-        console.log(token);
-        //response
-        res.send.token;
-        if (error) {
-            res.send('404 Error');
+    //try {
+        var login =req.body.login;
+        var motdepasse = req.body.motdepasse;
+        //console.log(login);
+        //console.log(motdepasse);
+        if ((login = 'user') && (motdepasse = 'user')) {
+            const token = jwt.sign({ user: req.body.login}, privateKey, { algorithm : 'RS256', expiresIn :'24h'});
+            //console.log(token);
+            //response
+            res.send.token;
+            if (error) {
+                res.send('404 Error');
+            } else {
+                res.send(200);
+            };
         } else {
-            res.send(200);
+            res.send('Les identifiants sont incorrects.');
         };
-    } else {
-        res.send('Les identifiants sont incorrects.');
-    };
+    //} catch {
+    //    res.status(400).send('Please check your variables and try again.')
+    //}
+
 });
 
 //fonction pour justify
@@ -54,6 +67,22 @@ function justify(){
 };
 
 
+//fonction pour vérifier le token 
+function verify(){
+    try { 
+        var verified = jwt.verify(token, privateKey, function(err, verified){
+            console.log('token vérifié')
+        });
+    
+        jwt.verify(token, 'wrong-secret', function(err, decoded) {
+            res.status(401).send('Utilisateur non authentifié')
+        });
+    } catch {
+        res.status(401).send('')
+    }
+};
+
+//Mettre le serveur sur écoute
 app.listen(8080, () => {
     console.log('Le serveur est sur écoute.')
 });
